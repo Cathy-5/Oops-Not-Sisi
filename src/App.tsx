d38046fcf0c9ca9e09bbc5de4d9bce47cabcb4e8
Sisi Wu <sw223hk@student.lnu.se>
2026-07-22 04:07:53 +0200

    show lst of sisi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css'
 
-const moles = [
+const holes = [
   { id: 0, imageid: '🐹' },
   { id: 1, imageid: '🐹' },
   { id: 2, imageid: '🐹' },
@@ -25,24 +25,24 @@ function Hole(props: { imageid: string; onClick: () => void }) {
 
 export default function App() {
   const [score, setScore] = useState(0);
-  const [sisiHoleId] = useState(() => Math.floor(Math.random() * 9));
-  const [live, setLive] = useState(3);
+  const [sisiHoleIds] = useState<number[]>([1, 5]); // ids of holes containing sisi: an array of numbers, now 1 and 5 shows sisi
+  const [livesCount, setLivesCount] = useState(3);
 
   function handleHit() {
     setScore(currentScore => currentScore + 1);
   }
 
   function handleLive() {
-    setLive(currentLive => Math.max(currentLive - 1, 0));
+    setLivesCount(currentLive => Math.max(currentLive - 1, 0));
   }
 
-  const listMoles = moles.map(mole => {
-    const isSisi = mole.id === sisiHoleId;
+  const listMoles = holes.map(hole => {
+    const isSisi =sisiHoleIds.includes(hole.id); // check if current hole's id inside Sisi List
 
     return (
       <Hole
-        key={mole.id}
-        imageid={isSisi ? '👧' : mole.imageid}
+        key={hole.id}
+        imageid={isSisi ? '👧' : hole.imageid}
         onClick={isSisi ? handleLive : handleHit}
       />
     );
@@ -52,7 +52,7 @@ export default function App() {
     <main>
       <header>
         <h1>Score: {score}</h1>
-        <h2>Lives: {lives.repeat(live)}</h2>
+        <h2>Lives: {lives.repeat(livesCount)}</h2>
       </header>
       <ul className='board'>{listMoles}</ul>
     </main>
